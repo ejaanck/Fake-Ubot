200365cc2c029a9a8115429f174196d1c281ca86
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-01-05 22:34:37 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/jonesroot/Key-Userbot.git"
-DIR="/root/jonesroot"
+REPO="https://github.com/ejaanck/Fake-Ubot.git"
+DIR="/root/ejaanck"
 
 spinner(){
     local pid=$!
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Key-Userbot ${BRANCH}... "
+    echo -e "Cloning FakeUbot ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
